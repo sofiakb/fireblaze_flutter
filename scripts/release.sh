8209ef7558936a40368de3d16daefe10fc86428f
Sofiakb <contact.sofiakb@gmail.com>
2024-05-11 15:17:08 +0200

    fix(global): update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,6 +8,6 @@ sed -i s/"version: $(grep 'version:' pubspec.yaml | head -1 | awk '{print $2}' |
 git add pubspec.yaml
 git commit -m "$(git log -1 --pretty=%B) - Release ${gitTag}"
 
-git push origin "release/${gitTag}" && git checkout - && git branch -D "release/${gitTag}"
+git push origin "release/${gitTag}" && git checkout - && git merge "release/${gitTag}" && git branch -D "release/${gitTag}"
 
 exit "${?}"
