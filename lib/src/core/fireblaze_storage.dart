a84e244247a710102377aae838a472f6179b2699
Sofiakb <contact.sofiakb@gmail.com>
2024-12-18 20:31:13 +0100

    update fireblaze_storage

--- lib/src/core/fireblaze_storage.dart
+++ lib/src/core/fireblaze_storage.dart
@@ -1,27 +1,44 @@
-import 'dart:io';
-
 import 'package:firebase_storage/firebase_storage.dart' as fire;
+import 'package:flutter/cupertino.dart';
 import 'package:mime/mime.dart';
-
-fire.FirebaseStorage storage = fire.FirebaseStorage.instance;
+import 'package:universal_io/io.dart';
 
 class FireblazeStorage {
-  static Future<fire.Reference> upload(
-      {required String destination, required File file}) async {
-    fire.Reference storageRef = storage.ref(destination);
+  final fire.FirebaseStorage _storage;
 
-    if (destination.contains('/')) {
-      List<String> splat = destination.split('/');
+  FireblazeStorage({fire.FirebaseStorage? storage})
+      : _storage = storage ?? fire.FirebaseStorage.instance;
 
-      storageRef = storage.ref(splat[0]);
+  Future<fire.Reference> upload({
+    required String destination,
+    required File file,
+  }) async {
+    try {
+      // Déterminer le type MIME
+      final contentType = lookupMimeType(file.path);
+      if (contentType == null) {
+        throw Exception("Impossible de déterminer le type MIME du fichier.");
+      }
 
-      splat.skip(1).forEach((pathSection) {
-        storageRef = storageRef.child(pathSection);
-      });
+      // Construire la référence à partir du chemin de destination
+      final storageRef = _storage.ref(destination);
+
+      // Mettre en ligne le fichier
+      final uploadTask = await storageRef.putFile(
+        file,
+        fire.SettableMetadata(contentType: contentType),
+      );
+
+      // Retourner la référence du fichier téléchargé
+      return uploadTask.ref;
+    } on fire.FirebaseException catch (e) {
+      // Gestion des erreurs Firebase
+      debugPrint('Erreur Firebase lors du téléchargement : ${e.message}');
+      rethrow;
+    } catch (e) {
+      // Gestion des autres erreurs
+      debugPrint('Erreur inattendue : $e');
+      rethrow;
     }
-
-    return (await storageRef.putFile(file,
-        fire.SettableMetadata(contentType: lookupMimeType(file.path))))
-        .ref;
   }
 }
